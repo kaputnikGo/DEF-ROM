358f8c8672dcb8a8eac0715550d2ead02f7cc589
kaputnikGo <10411004+kaputnikGo@users.noreply.github.com>
2021-03-19 19:33:31 +1100

    write error fix
    addr 09 cause write errors over program code

--- StellarWars-SYNTH7-demo.asm
+++ StellarWars-SYNTH7-demo.asm
@@ -16,41 +16,40 @@
 ;*************************************;
 ; USED RAM ADDR LOCATIONS (typical values)
 ;*************************************;
-0000 : nn nn                          ; ?, B
+0000 : DA nn                          ; ?, B
 0002 : nn nn                          ; X
-0004 : nn nn                          ; A, B
+0004 : nn nn                          ; A, B 
 0006 : nn 80                          ; B countdown, ?
-0008 : 01 nn                          ; X count up 0182 - 0140 changes values in 00-03
+0008 : 00 nn                          ; X,
 000A : nn nn                          ; X,B
-000C : nn 00                          ; PIA1 store 00-02 pitch/loop up/dwn speed slow to faster
-000E : FF                             ; PIA2, makes good sound
+000C : 00 00                          ; PIA1,PIA2 store 00-02 pitch/loop up/dwn speed slow to faster
+000E : 00 00                          ;not used
 ;*************************************;
-;RESET INIT (POWER-ON) org 000F
+;RESET INIT (POWER-ON) org 0010
+;*************************************; 
+0010 : 8E 01 FF   lds #$01FF          ; load SP with 01FFh
+0013 : CE 80 00	  ldx #$8000          ; load X with 8000h, PIA1 (DAC) addr
+0016 : 6F 02      clr $02,x           ; clear(00) addr X + 02h (set 8002 PIA1 PR/DDR port B in)
+0018 : 86 FF      ldaa  #$FF          ; load A with FFh (1111 1111)
+001A : A7 00      staa  $00,x         ; store A in addr X + 00h (set 8000 PIA1 PR/DDR port A out)
+001C : 86 3C      ldaa  #$3C          ; load A with 3Ch(0011 1100)
+001E : A7 01      staa  $01,x         ; store A in addr X + 01h (8001 PIA1 CR port A)
+0020 : 86 37      ldaa  #$37          ; load A with 37h(0011 0111)
+0022 : A7 03      staa  $03,x         ; store A in addr X + 03h (8003 PIA1 CR port B) 
+0024 : 7F 40 02   clr X4002           ; clear(00) 4002h (set PIA2 PR/DDR port B in)
+0027 : 86 04      ldaa  #$04          ; set CR bit 2 high for PIA2
+0029 : B7 40 03   staa X4003          ; store A in addr 4003 (PIA2 CR port B)
+002C : 7F 00 09   clr X0009           ;clr (00) addr 0009, else ram writes over program code
 ;*************************************;
-000F : 8E 01 FF   lds #$01FF          ; load SP with 01FFh
-0012 : CE 80 00	  ldx #$8000          ; load X with 8000h, PIA1 (DAC) addr
-0015 : 6F 02      clr $02,x           ; clear(00) addr X + 02h (set 8002 PIA1 PR/DDR port B in)
-0017 : 86 FF      ldaa  #$FF          ; load A with FFh (1111 1111)
-0019 : A7 00      staa  $00,x         ; store A in addr X + 00h (set 8000 PIA1 PR/DDR port A out)
-001B : 86 3C      ldaa  #$3C          ; load A with 3Ch(0011 1100)
-001D : A7 01      staa  $01,x         ; store A in addr X + 01h (8001 PIA1 CR port A)
-001F : 86 37      ldaa  #$37          ; load A with 37h(0011 0111)
-0021 : A7 03      staa  $03,x         ; store A in addr X + 03h (8003 PIA1 CR port B) 
-0023 : 7F 40 02   clr X4002           ; clear(00) 4002h (set PIA2 PR/DDR port B in)
-0026 : 86 04      ldaa  #$04          ; set CR bit 2 high for PIA2
-0028 : B7 40 03   staa X4003          ; store A in addr 4003 (PIA2 CR port B)
+;IRQ - 002F
 ;*************************************;
-;IRQ - MAIN LOOP 002B
-;*************************************;
-002B : B6 80 02   ldaa  $8002         ;load A with PIA1 B
-002E : 97 0C      staa  $0C           ;store A in addr 0C
-0030 : 01         nop                 ; 
+002F : 01 01      nop nop             
 0031 : CE 00 00   ldx	#$0000          ;load X with 0000h
 0034 : DF 02      stx	X0002           ;store X in addr 02
 0036 : C6 AF      ldab	#$AF          ;load B with AFh (1010 1111)
 0038 : D7 01      stab	X0001         ;store B in addr 01
 ;*************************************;
-;SYNTH7
+;SYNTH7 main loop
 ;*************************************;
 003A : 84 0F      anda	#$0F          ;and A with 0Fh (0000 1111) 1+1=1, all else =0
 003C : CE 01 33   ldx	#$0133          ;load X with 0133h (VSYN7 data 2)
