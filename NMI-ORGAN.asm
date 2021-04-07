a9edf5fec8e3a6f057619965338e2cee3cf4156c
kaputnikGo <10411004+kaputnikGo@users.noreply.github.com>
2021-04-07 12:44:12 +1000

    final mem and fdb
    fdb melody tables sectioned and recorded, final mem save of 3 bytes

--- NMI-ORGAN.asm
+++ NMI-ORGAN.asm
@@ -1,4 +1,4 @@
-; DEF-ROM NMI-ORGAN SOUNDS CODE - 6 Apr 2021
+; DEF-ROM NMI-ORGAN SOUNDS CODE - 7 Apr 2021
 ; hack for Heathkit ET-3400 Audio Setup
 ; user RAM = 196 + 255 bytes = 453
 ; addr 0000 - 00C4 and 0100 - 01FF
@@ -8,9 +8,6 @@
 ; using edited subroutines NMI, PARAM7, CALCOS, SYNTH8 - revert mem locations.
 ;
 ; Full NMI reset is 1st 2 bars JS Bach's Toccata and Fugue in D Minor + Williams Boot Carpet
-; orig starts at last half 1st bar, repeat lower octave, then no more valid melody FDB
-; current: A2, G2, A2, E2, F2, C#2, D2 in part 1, other parts below
-; note: 01BE change from F8 to CC for last jmp write location means mem length broken here.
 ; duration (X count) + pitch (NOP) values
 ; added SoundROM6 melody FDB for testing
 ; Audacity playback wav speed x2.25 to get approx
@@ -92,74 +89,75 @@
 ;*************************************;
 ;PRM71 0100
 ;*************************************;
-0100 : 7F 00 04   clr $0004           ;clr (00) addr 04 <-- not used?
-0103 : 97 0D      staa  $0D           ;store A in addr 0D
-0105 : CE 01 93   ldx #$0193          ;load X with 0193 (addr of melody data)
+0100 : 97 0D      staa  $0D           ;store A in addr 0D
+0102 : CE 01 90   ldx #$0190          ;load X with 0190 (addr of melody data) 
 ;PRM72
-0108 : A6 00      ldaa  $00,x         ;load A with value at addr X + 00h
-010A : 27 2E      beq L0083           ;branch if Z=1(in accum all bits are 0) to PRM75
-010C : 7A 00 0D   dec $000D           ;decr value in addr 0D
-010F : 27 06      beq L0061           ;branch if Z=1 PRM73
-0111 : 4C         inca                ;incr A
-0112 : BD 00 B2   jsr L00B2           ;jump sub CALCOS (shifts X start addr 0193 to 01A0)
-0115 : 20 F1      bra L0052           ;branch always PRM72
+0105 : A6 00      ldaa  $00,x         ;load A with value at addr X + 00h
+0107 : 27 2E      beq L0083           ;branch if Z=1(in accum all bits are 0) to PRM75
+0109 : 7A 00 0D   dec $000D           ;decr value in addr 0D
+010C : 27 06      beq L0061           ;branch if Z=1 PRM73
+010E : 4C         inca                ;incr A
+010F : BD 00 B2   jsr L00B2           ;jump sub CALCOS (shifts X start addr 0190 to 019D)
+0112 : 20 F1      bra L0052           ;branch always PRM72
 ;PRM73
-0117 : 08         inx                 ;incr X
-0118 : DF 0B      stx $0B             ;store X in addr 0B
-011A : BD 00 B2   jsr L00B2           ;jump sub CALCOS
-011D : DF 09      stx $09             ;store X in addr 09
-011F : DE 0B      ldx $0B             ;load X in addr 0B
+0114 : 08         inx                 ;incr X
+0115 : DF 0B      stx $0B             ;store X in addr 0B
+0117 : BD 00 B2   jsr L00B2           ;jump sub CALCOS
+011A : DF 09      stx $09             ;store X in addr 09
+011C : DE 0B      ldx $0B             ;load X in addr 0B
 ;PRM74 - store melody location into mem 11, sets X with melody addr and A with X(lo)
-0121 : A6 00      ldaa  $00,x         ;load A with addr X + 00h
-0123 : 97 11      staa  X0011         ;store A in addr 11
-0125 : A6 01      ldaa  $01,x         ;load A with addr X + 01h
-0127 : EE 02      ldx $02,x           ;load X with addr X + 02h
-0129 : DF 0F      stx $0F             ;store X in addr 0F
-012B : BD 01 3B   jsr L013B           ;jump sub SYNTH8 013B
-012E : DE 0B      ldx $0B             ;load X with addr 0B
+011E : A6 00      ldaa  $00,x         ;load A with addr X + 00h
+0120 : 97 11      staa  X0011         ;store A in addr 11
+0122 : A6 01      ldaa  $01,x         ;load A with addr X + 01h
+0124 : EE 02      ldx $02,x           ;load X with addr X + 02h
+0126 : DF 0F      stx $0F             ;store X in addr 0F
+0128 : BD 01 38   jsr L0138           ;jump sub SYNTH8 0138
+012B : DE 0B      ldx $0B             ;load X with addr 0B
+012D : 08         inx                 ;incr X
+012E : 08         inx                 ;incr X
+012F : 08         inx                 ;incr X
 0130 : 08         inx                 ;incr X
-0131 : 08         inx                 ;incr X
-0132 : 08         inx                 ;incr X
-0133 : 08         inx                 ;incr X
-0134 : DF 0B      stx $0B             ;store X in addr 0B
-0136 : 9C 09      cpx $09             ;comp X with addr 09
-0138 : 26 E7      bne L0121           ;branch Z=0 PRM74
+0131 : DF 0B      stx $0B             ;store X in addr 0B
+0133 : 9C 09      cpx $09             ;comp X with addr 09
+0135 : 26 E7      bne L0121           ;branch Z=0 PRM74
 ;PRM75
-013A : 39         rts                 ;return subroutine
+0137 : 39         rts                 ;return subroutine
 ;*************************************;
-;SYNTH8 013B
+;SYNTH8 0138
 ;*************************************;
-013B : CE 00 12   ldx #$0012          ;load X with 0012h flood start
-013E : 80 02      suba  #$02          ;A = A - 02h (0000 0010)
+0138 : CE 00 12   ldx #$0012          ;load X with 0012h flood start
+013B : 80 02      suba  #$02          ;A = A - 02h (0000 0010)
 ;SYN81 - 01 nop length writer for freq/pitch
-0140 : 23 15      bls L0157           ;branch if lower or same(C and Z = 1) SYN83 <-- write loop from 0012
-0142 : 81 03      cmpa  #$03          ;compare A with 03h (0000 0011) (needs 3 more bytes space for jmp write)
-0144 : 27 09      beq L014F           ;branch Z=1 SYN82 <-- loop countdown =0
-0146 : C6 01      ldab  #$01          ;load B with 01h (0000 0001)
-0148 : E7 00      stab  $00,x         ;store B in addr X + 00h <-- 01 nop writer
-014A : 08         inx                 ;incr X
-014B : 80 02      suba  #$02          ;A = A - 02h (0000 0010)
-014D : 20 F1      bra L0140           ;branch always SYN81
+013D : 23 15      bls L0154           ;branch if lower or same(C and Z = 1) SYN83 <-- write loop from 0012
+013F : 81 03      cmpa  #$03          ;compare A with 03h (0000 0011) (needs 3 more bytes space for jmp write)
+0141 : 27 09      beq L014C           ;branch Z=1 SYN82 <-- loop countdown =0
+0143 : C6 01      ldab  #$01          ;load B with 01h (0000 0001)
+0145 : E7 00      stab  $00,x         ;store B in addr X + 00h <-- 01 nop writer
+0147 : 08         inx                 ;incr X
+0148 : 80 02      suba  #$02          ;A = A - 02h (0000 0010)
+014A : 20 F1      bra L013D           ;branch always SYN81
 ;SYN82 - writes 91 00 (cmpa $00) gate for freq/pitch end jmp
-014F : C6 91      ldab  #$91          ;load B with 91h (1001 0001)
-0151 : E7 00      stab  $00,x         ;store B in addr X + 00h
-0153 : 6F 01      clr $01,x           ;clear addr X + 01h
-0155 : 08         inx                 ;incr X       
-0156 : 08         inx                 ;incr X
-;SYN83 - writes 7E 01 65 (jmp 0165) end freq/pitch
-0157 : C6 7E      ldab  #$7E          ;load B with 7Eh (1111 1110)
-0159 : E7 00      stab  $00,x         ;store B in addr X + 00h
-015B : C6 01      ldab  #$01          ;load B with 01h (0000 0001)
-015D : E7 01      stab  $01,x         ;store B in addr X + 01h
-015F : C6 65      ldab  #$65          ;load B with 65h (1110 0010) 
-0161 : E7 02      stab  $02,x         ;store B in addr X + 02h
-0163 : DE 0F      ldx $0F             ;load X with addr 0F
-;0165 - synth output writer and loop reader
-0165 : 4F         clra                ;clear A
-0166 : F6 00 0E   ldab  $000E         ;load B with addr 0E
-0169 : 5C         incb                ;incr B
-016A : D7 0E      stab  $0E           ;store B in addr 0E <-- counter up
-016C : D4 11      andb  $11           ;and B with value in addr 11 (3E in 1st run)
+014C : C6 91      ldab  #$91          ;load B with 91h (1001 0001)
+014E : E7 00      stab  $00,x         ;store B in addr X + 00h
+0150 : 6F 01      clr $01,x           ;clear addr X + 01h
+0152 : 08         inx                 ;incr X       
+0153 : 08         inx                 ;incr X
+;SYN83 - writes 7E 01 62 (jmp 0162) end freq/pitch
+0154 : C6 7E      ldab  #$7E          ;load B with 7Eh (1111 1110)
+0156 : E7 00      stab  $00,x         ;store B in addr X + 00h
+0158 : C6 01      ldab  #$01          ;load B with 01h (0000 0001)
+015A : E7 01      stab  $01,x         ;store B in addr X + 01h
+015C : C6 62      ldab  #$62          ;load B with 62h (0110 0010) 
+015E : E7 02      stab  $02,x         ;store B in addr X + 02h
+0160 : DE 0F      ldx $0F             ;load X with addr 0F
+;0162 - synth output writer and loop reader
+0162 : 4F         clra                ;clear A
+0163 : F6 00 0E   ldab  $000E         ;load B with addr 0E
+0166 : 5C         incb                ;incr B
+0167 : D7 0E      stab  $0E           ;store B in addr 0E <-- counter up
+0169 : D4 11      andb  $11           ;and B with value in addr 11 (3E in 1st run)
+016B : 54         lsrb                ;logic shift right B (bit7=0)
+016C : 89 00      adca  #$00          ;A = Carry + A + 00h 
 016E : 54         lsrb                ;logic shift right B (bit7=0)
 016F : 89 00      adca  #$00          ;A = Carry + A + 00h 
 0171 : 54         lsrb                ;logic shift right B (bit7=0)
@@ -170,182 +168,84 @@
 0178 : 89 00      adca  #$00          ;A = Carry + A + 00h 
 017A : 54         lsrb                ;logic shift right B (bit7=0)
 017B : 89 00      adca  #$00          ;A = Carry + A + 00h 
-017D : 54         lsrb                ;logic shift right B (bit7=0)
+017D : 54         lsrb                ;logic shift right B (bit7=0) (shift down till C set for adca to count up, A to DAC)
 017E : 89 00      adca  #$00          ;A = Carry + A + 00h 
-0180 : 54         lsrb                ;logic shift right B (bit7=0) (shift down till C set for adca to count up, A to DAC)
-0181 : 89 00      adca  #$00          ;A = Carry + A + 00h 
-0183 : 1B         aba                 ;A = A + B 
+0180 : 1B         aba                 ;A = A + B 
+0181 : 48         asla                ;arith shift left A (bit0 is 0)
+0182 : 48         asla                ;arith shift left A (bit0 is 0)
+0183 : 48         asla                ;arith shift left A (bit0 is 0)
 0184 : 48         asla                ;arith shift left A (bit0 is 0)
-0185 : 48         asla                ;arith shift left A (bit0 is 0)
-0186 : 48         asla                ;arith shift left A (bit0 is 0)
-0187 : 48         asla                ;arith shift left A (bit0 is 0)
-0188 : 48         asla                ;arith shift left A (bit0 is 0) (increases value in A)
-0189 : B7 80 00   staa  $8000         ;store A in DAC output SOUND
-018C : 09         dex                 ;decr X
-018D : 27 03      beq  L019F          ;branch Z=1 SYN84
-018F : 7E 00 12   jmp  L0012          ;jump to timer location 0012 (to jmp writes that set freq/pitch duration)
+0185 : 48         asla                ;arith shift left A (bit0 is 0) (increases value in A)
+0186 : B7 80 00   staa  $8000         ;store A in DAC output SOUND
+0189 : 09         dex                 ;decr X
+018A : 27 03      beq  L018F          ;branch Z=1 SYN84
+018C : 7E 00 12   jmp  L0012          ;jump to timer location 0012 (to jmp writes that set freq/pitch duration)
 ;SYN84
-0192 : 39         rts                 ;return subroutine
+018F : 39         rts                 ;return subroutine
 ;*************************************;
-;ORGAN 0193 melody table (total FDB 162 bytes, A2h length)
+;ORGAN INIT 0190 (incl. offset)
 ;*************************************;
-0193 : 0C 7F 1D 0F FB 7F 23 0F        ;notes on fdb below
-019B : 15 FE 08 50 8A 88 3E 3F        ;alt fdb tables below
-01A3 : 02 3E 7C 04 03 FF 3E 3F        ;
-01AB : 2C E2 7C 12 0D 74 7C 0D        ;
-01B3 : 0E 41 7C 23 0B 50 7C 1D        ;
-01BB : 29 F2 7C 3F 02 3E;F8 04        ;
-01C1 :                                ;
-;01C4 end                             ; 
+0190 : 02 7F 8A 88                    ; PRM72/CALCOS offset calc +2, move 12 bytes for last 3 notes to part 4
 ;*************************************;
-;melody fdb X pairs for freq and length
+;ORGAN 0194 melody tables, SoundROM1 NMI
 ;*************************************;
-; [pitch:nop length:X]
-0193 : [0C [7F] 1D 0F FB] [7F 23 0F          ;PRM72/CALCOS X=(01(A=0C++ ADDA 93h)++) ie. start X=0193 then X=01A1
-019B : 15] [FE 08 50 8A] [88] | [3E 3F]      ;11 bytes skipped to [88] called via CALCOS ldx $09 (01A0), 01A1 is 1st note
-01A3 : [02 3E] | [7C 04] [03 FF] | [3E 3F]   ; 
-01AB : [2C E2] | [7C 12] [0D 74] | [7C 0D]   ;
-01B3 : [0E 41] | [7C 23] [0B 50] | [7C 1D]   ;
-01BB : [29 F2] | [7C 3F] [02 3E] |           ;
+;[pitch:nop length:X]                 ;note values
+0194 : [3E 3F 02 3E][7C 04 03 FF]     ;PART 1
+019C : [3E 3F 2C E2][7C 12 0D 74]
+01A4 : [7C 0D 0E 41][7C 23 0B 50]
+01AC : [7C 1D 29 F2][7C 3F 02 3E]
+01B4 : [F8 04 03 FF][7C 3F 2C E2]
+01BC : [F8 12 0D 74][F8 0D 0E 41]
+;01C4 : end
+[F8 23 0B 50][F8 1D 2F F2]            ;PART 2
+[F8 23 05 A8][F8 12 06 BA]
+[F8 04 07 FF][7C 37 04 C1]
+[7C 23 05 A8][7C 12 06 BA]
+[3E 04 07 FF][3E 37 04 C1]
+[3E 23 05 A8][1F 12 06 BA]
+;
+[1F 04 07 FF][1F 37 04 C1]            ;PART 3  
+[1F 23 16 A0][FE 1D 17 F9] 
+[7F 37 13 06][7F 3F 08 FA]
+[FE 04 0F FF][FE 0D 0E 41] 
+[FE 23 0B 50][FE 1D 5F E4]
+[00 47 3F 37][30 29 23 1D]        
+;
+[17 12 0D 08][7F 1D 0F FB]            ;PART 4 (taken from start of FDB table)
+[7F 23 0F 15][FE 08 50 8A]
 ;*************************************;
-; ALT OFFSET +2 fdb table - works
+; FDB table from Pharaoh ROM Sound7
 ;*************************************;
-0193 : [02 7F] 8A [88]|[3E 3F 02 3E] |      ; filler 8A (padding) is skipped, added 10 bytes mem here
-019B : [7C 04 03 FF] |[3E 3F 2C E2] |      ;
-01A3 : [7C 12 0D 74] |[7C 0D 0E 41] |      ;
-01AB : [7C 23 0B 50] |[7C 1D 29 F2] |      ;
-01B3 : [7C 3F 02 3E] |[F8 04 03 FF] |      ; 1 added note
-01BB : [7C 3F 2C E2] |[F8 12 0D 74] |      ; 2 added notes
-;01C3 : end
+0194 : [7C 29 05 56][7C 1D 05 FE]     ;PART 1
+019C : [7C 17 0C B2][7C 1D 0B FC]
+01A4 : [7C 29 05 56][F8 04 07 FF]
+01AC : [7C 29 05 56][7C 1D 05 FE]
+01B4 : [7C 17 06 59][7C 04 07 FF]
+01BC : [7C 1D 05 FE][7C 17 06 59]
+;01C4 end
+[7C 29 2A B6]                         ;PART 2
 ;*************************************;
-; ALT OFFSET +2 fdb table & 1/2 NOP (hi,lo) ~= 12 semitones (on Heathkit hardware clock)
+; melody from Pharaoh ROM Sound24
 ;*************************************;
-0193 : [02 7F] 8A [88]|[1F 1F 02 3E] |      ; filler 8A (padding) is skipped, added 10 bytes mem here
-019B : [3E 02 03 FF] |[1F 1F 2C E2] |      ;
-01A3 : [3E 09 0D 74] |[3E 06 0E 41] |      ;
-01AB : [3E 11 0B 50] |[3E 0E 29 F2] |      ;
-01B3 : [3E 1F 02 3E] |[7C 02 03 FF] |      ; 1 added note
-01BB : [3E 1F 2C E2] |[7C 09 0D 74] |      ; 2 added notes
-;01C3 : end
-;*************************************;
-; ALT OFFSET +2 fdb table & 1/2 X (hi,lo) counter
-;*************************************;
-0193 : [02 7F] 8A [88]|[3E 3F 01 1F] |      ; filler 8A (padding) is skipped, added 10 bytes mem here, 0197 :notes
-019B : [7C 04 01 FF] |[3E 3F 16 71] |      ;
-01A3 : [7C 12 06 BA] |[7C 0D 07 20] |      ;
-01AB : [7C 23 05 A8] |[7C 1D 14 F9] |      ;
-01B3 : [7C 3F 01 1F] |[F8 04 01 1F] |      ; 1 added note
-01BB : [7C 3F 16 71] |[F8 12 06 BA] |      ; 2 added notes
-;01C3 : end
-;*************************************;
-; ALT OFFSET melody from Pharaoh ROM
-;*************************************;
-0193 : [02 7F]8A [88]| [7C 29 05 56] |    ;+2 offset, 8A padding, [A=88], 1st note
-019B : [7C 1D 05 FE] | [7C 17 0C B2] |    ;
-01A3 : [7C 1D 0B FC] | [7C 29 05 56] |    ;
-01AB : [F8 04 07 FF] | [7C 29 05 56] |    ;
-01B3 : [7C 1D 05 FE] | [7C 17 06 59] |    ;
-01BB : [7C 04 07 FF] | [7C 1D 05 FE] |    ;
-;FEFD : 59]
-;*************************************;
-;original ROM1:
-;*************************************;
-;called by NMI -> PARAM7
-FD94 : 0C 7F 1D 0F FB 7F 23 0F
-FD9C : 15 FE 08 50 8A 88 3E 3F
-FDA4 : 02 3E 7C 04 03 FF 3E 3F
-FDAC : 2C E2 7C 12 0D 74 7C 0D
-FDB4 : 0E 41 7C 23 0B 50 7C 1D 
-FDBC : 29 F2 7C 3F 02 3E F8 04
-FDC4 : 03 FF 7C 3F 2C E2 F8 12 
-FDCC : 0D 74 F8 0D 0E 41 F8 23 
-FDD4 : 0B 50 F8 1D 2F F2 F8 23
-FDDC : 05 A8 F8 12 06 BA F8 04
-FDE4 : 07 FF 7C 37 04 C1 7C 23 
-FDEC : 05 A8 7C 12 06 BA 3E 04
-FDF4 : 07 FF 3E 37 04 C1 3E 23 
-FDFC : 05 A8 1F 12 06 BA 1F 04
-FE04 : 07 FF 1F 37 04 C1 1F 23 
-FD0C : 16 A0 FE 1D 17 F9 7F 37
-FE14 : 13 06 7F 3F 08 FA FE 04
-FE1C : 0F FF FE 0D 0E 41 FE 23 
-FE24 : 0B 50 FE 1D 5F E4 00 47
-FE2C : 3F 37 30 29 23 1D 17 12
-FE34 : 0D 08 04
-;FE36 - FD94 = A2h (162 bytes)
-;*************************************;
-; SoundROM 6 (Pharaoh) melody fdb
-;*************************************;
-FECD : 34                            ;offset 34h not used
-FECE : [7C 29 05 56][7C 1D 05 FE]    ;Sound7 start part1
-FED6 : [7C 17 0C B2][7C 1D 0B FC]
-FEDE : [7C 29 05 56][F8 04 07 FF] 
-FEE6 : [7C 29 05 56][7C 1D 05 FE] 
-FEEE : [7C 17 06 59][7C 04 07 FF] 
-FEF6 : [7C 1D 05 FE]                 ;end Sound7 part1
-FEFA : 
-;unknown below
-7C 17 
-06 59 7C 29 
-2A B6 18 F8 04 02 FF 00
-23 06 01 F8 04 03 FF 00 
-23 02 AB F8 04 07 FF 7C 
-29 15 5B 60
-;end unknown 
-[7C 1D 05 FE][F8 04 1F FF]   ;Sound24 start
-[7C 04 1F FF][7C 1D 11 FA]   
-[00 1D 02 FF][7C 1D 02 FF] 
-[7C 17 0C B2][7C 1D 0B FC]
-[7C 23 0A AD][7C 37 09 83] 
-[7C 3F 11 F5][3E 3F 11 F5]
-[7C 17 16 34][7C 1D 02 FF]
+0194 : [7C 1D 05 FE][F8 04 1F FF]     ;PART 1
+019C : [7C 04 1F FF][7C 1D 11 FA]   
+01A4 : [00 1D 02 FF][7C 1D 02 FF] 
+01AC : [7C 17 0C B2][7C 1D 0B FC]
+01B4 : [7C 23 0A AD][7C 37 09 83] 
+01BC : [7C 3F 11 F5][3E 3F 11 F5]
+;01C4 end
+[7C 17 16 34][7C 1D 02 FF]            ;PART 2
 [7C 17 0C B2][7C 1D 0B FC]
 [7C 29 0A AD][7C 3F 04 7D]
 [7C 37 04 C1][7C 3F 14 36]
 [F8 1D 14 FF][F8 04 03 FF]
-[00 04 03 F8][F8 04 3F FF]   ;end Sound24
+[00 04 03 F8][F8 04 3F FF]            ;end Sound24
 ;*************************************;
-;note mapping ROM for recording from hardware (small ram alloc)
+; melody from Pharaoh ROM Sound22
 ;*************************************;
-FDA2 : 3E 3F 02 3E  ;start part1
-FDA6 : 7C 04 03 FF 
-FDAA : 3E 3F 2C E2 
-FDAE : 7C 12 0D 74 
-FDB2 : 7C 0D 0E 41 
-FDB6 : 7C 23 0B 50 
-FDBA : 7C 1D 29 F2  ;end part1
-FDBE : 7C 3F 02 3E  ;start part2
-FDC2 : F8 04 03 FF 
-FDC6 : 7C 3F 2C E2 
-FDCA : F8 12 0D 74 
-FDCE : F8 0D 0E 41 
-FDD2 : F8 23 0B 50 
-FDD6 : F8 1D 2F F2  ;end part2
-FDDA : F8 23 05 A8  ;start part3
-FDDE : F8 12 06 BA
-FDE2 : F8 04 07 FF 
-FDE6 : 7C 37 04 C1 
-FDEA : 7C 23 05 A8 
-FDEE : 7C 12 06 BA 
-FDF2 : 3E 04 07 FF 
-FDF6 : 3E 37 04 C1  ;end part3
-FDFA : 3E 23 05 A8  ;start part4
-FDFE : 1F 12 06 BA 
-FE02 : 1F 04 07 FF 
-FE06 : 1F 37 04 C1 
-FE0A : 1F 23 16 A0 
-FE0E : FE 1D 17 F9 
-FE12 : 7F 37 13 06 
-FE16 : 7F 3F 08 FA  ;end part4 
-FE1A : FE 04 0F FF  ;start part5
-FE1E : FE 0D 0E 41 
-FE22 : FE 23 0B 50 
-FE26 : FE 1D 5F E4 
-FE2A : 00 47 3F 37 
-FE2E : 30 29 23 1D 
-FE32 : 17 12 0D 08  ;end part5
-;
-FD95 : 7F 1D 0F FB  ;start part6, last 3 notes, stored at start of FDB table...
-FD99 : 7F 23 0F 15 
-FD9D : FE 08 50 8A  ;end part6
+;18                                   ; ? 
+0194 : [F8 04 02 FF][00 23 06 01]     ;notes
+019C : [F8 04 03 FF][00 23 02 AB] 
+01A4 : [F8 04 07 FF][7C 29 15 5B]
+;60                                   ; ?
 ;*************************************;
